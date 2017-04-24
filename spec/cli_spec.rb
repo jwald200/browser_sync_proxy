a6c18827877bc532dc41384f1b061b13d8df9e14
jwald1 <email@example.com>
2017-04-23 20:07:21 -0400

    starts cli_spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,3 +1,24 @@
 describe BrowserSyncProxy::CLI do
+  describe '#setup' do
+    it 'starts the setup' do
+      setup = BrowserSyncProxy::Setup.new
+      expect(setup).to receive(:run)
+      expect(subject).to receive(:setup)
+      subject.setup
+    end
+  end
 
+  xdescribe '#start' do
+    it 'errors if no config found' do
+
+    end
+
+    it 'starts browser-sync with sinatra defaults if --sinatra given' do
+
+    end
+
+    it 'starts browser-sync with rails defaults if --rails given' do
+
+    end
+  end
 end
